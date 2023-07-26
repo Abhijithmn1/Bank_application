75cadde71b436bd39967ad569179f243fcaf67a6
ABHIJITH M N <134260153+Abhijithmn1@users.noreply.github.com>
2023-07-26 15:27:23 +0530

    Update urls.py

--- zid_proj/bank_app/urls.py
+++ zid_proj/bank_app/urls.py
@@ -5,5 +5,5 @@ app_name = 'bank_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('form', views.form_page, name='form_page'),
-    path('submit', views.submit_form, name='submit_form'),
+    path('confirmation/', views.confirmation, name='confirmation'),
     ]
